c330d3297f146b88d74812c029135a3a30da321e
srkemper <srkemper@stanford.edu>
2014-02-05 23:42:19 -0800

    Got everything working with express, handlebars, etc. Can now set up routes and start working on main pages. about to add bootstrap to the project

--- smilendar/app.js
+++ smilendar/app.js
@@ -8,13 +8,15 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var handlebars = require('express3-handlebars');
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
